028b2dd36963b21360d4dd28cbd5caef4766152a
Andrei Novikov <scanyv@yandex.ru>
2023-02-01 21:05:53 +0300

    Working version

---
+++ statistic/statistic-main/src/main/java/ru/practicum/StatisticMainClass.java
@@ -0,0 +1,13 @@
+package ru.practicum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+
+@SpringBootApplication
+@EnableCaching
+public class StatisticMainClass {
+    public static void main(String[] args) {
+        SpringApplication.run(StatisticMainClass.class, args);
+    }
+}
---
+++ statistic/statistic-main/src/main/java/ru/practicum/stat/StatisticController.java
@@ -0,0 +1,39 @@
+package ru.practicum.stat;
+
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.dto.StatisticDto;
+import ru.practicum.dto.StatisticDtoIncom;
+import ru.practicum.stat.model.Statistic;
+
+import java.util.Collection;
+
+@Controller
+@RequestMapping
+@RequiredArgsConstructor
+@Slf4j
+@Validated
+public class StatisticController {
+    private final StatisticService statisticService;
+
+    @GetMapping("/stats")                                    // метод получения пользователя по Id
+    public ResponseEntity<Collection<StatisticDto>> getStats(@NonNull @RequestParam String start,
+                                                             @NonNull @RequestParam String end,
+                                                             @NonNull @RequestParam String[] uris,
+                                                             @RequestParam(defaultValue = "false") boolean unique) {
+        log.info("Get stats with start={}, end={}, uris={}, unique={}", start, end, uris, unique);
+        return new ResponseEntity<>(statisticService.getStats(start, end, uris, unique), HttpStatus.OK);
+    }
+
+    @PostMapping("/hit")                         // метод добавления пользователя
+    public ResponseEntity<Statistic> postStat(@RequestBody StatisticDtoIncom statistic) {
+        log.info("Creating stat record {}", statistic);
+        return new ResponseEntity<>(statisticService.postStat(statistic), HttpStatus.OK);
+    }
+}
